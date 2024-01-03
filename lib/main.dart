52ffe3046bf0aac1b93b23b823de86da93861e81
Erika <erika@nearcoast.com>
2024-01-03 12:50:37 +0200

    fixed login with email on Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_home/page/authorization/login_&_reg.dart';
-import 'package:firebase_home/page/authorization/login_page.dart';
 import 'package:firebase_home/service/auth/intro_page.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   runApp(const MyApp());
 }
 
@@ -20,7 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      // home: LoginPage(),
       home: IntroPage(),
     );
   }
